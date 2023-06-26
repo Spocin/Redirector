ebba4df6c75297f1995d04cf2d9cb9b0f95aa8b8
Spocin <michal.spocinski.01@gmail.com>
2023-06-27 00:09:59 +0200

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HttpClient} from "@angular/common/http";
+import {IRoute} from "./interfacces/route.interface";
+import {map} from "rxjs";
+import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'Redirector';
+
+  private readonly http = inject(HttpClient);
+
+  protected getRoutes$ = this.http.get("assets/routes.json").pipe(
+    map((res: any) => res.routes as IRoute[])
+  );
+
+  protected navigateTo(url: string) {
+    window.location.href = url;
+  }
 }
---
+++ src/app/interfacces/route.interface.ts
@@ -0,0 +1,6 @@
+export interface IRoute {
+  name: string;
+  description: string;
+  url: string;
+  hexColor: string;
+}
